b3dcfe770383fe8c706aca1f41d819125b1fa67a
xavannahb <xavannahbaars@icloud.com>
2026-02-04 12:21:59 +0100

    request 2

--- main.js
+++ main.js
@@ -5,16 +5,27 @@ const parentElement = document.querySelector('main');
 
 parentElement.classList.add('loading');
 
-fetchJson(apiURL).then(function (response){
-    parentElement.innerHTML = `
-        <article>
-        <h2>${response.data.name}</h2>
-        <p>${response.data.nickname}</p>
-        <p>${response.data.birthdate}</p>
-        </article>
-    `
+fetchJson(apiURL).then(({data}) => {
+    data.custom = JSON.parse(data.custom);
+
+    parseCard(data, parentElement); 
     parentElement.classList.remove('loading');
 })
+
+function parseCard(userData, targetElement) {
+    targetElement.innerHTML = `
+        <article>
+        <h2>${userData.name}</h2>
+        <p>${userData.nickname}</p>
+        <p>${userData.birthdate}</p>
+        <table>
+        <tr>
+        <td>Schoenmaat:</td><td>${userData.birthdate}</td>
+        </tr>
+        </table>
+        </article>
+    `
+}
 
 async function fetchJson(url, payload = {}) {
   return await fetch(url, payload)
